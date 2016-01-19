badbb705d23691b1260b2ea49ecccebd1907558f
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:52:20 -0800

    🛠 Added VERSION, requirements.txt

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.getty.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+desc = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.getty',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.getty'],
-    version='0.03',
-    description='Python tools for working with Getty controlled vocabularies (and Who\'s On Firstdata)',
+    version=version,
+    description='Python tools for working with Getty controlled vocabularies (and Who\'s On First data)',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-getty',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-getty',
     install_requires=[
         'requests',
         'rdflib',
         'geojson',
         'atomicwrites',
-        'mapzen.whosonfirst.geojson>=0.03',
+        'mapzen.whosonfirst.geojson>=0.06',
         ],
     dependency_links=[
-        'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.03',
+        'https://github.com/whosonfirst/py-mapzen-whosonfirst-geojson/tarball/master#egg=mapzen.whosonfirst.geojson-0.06',
         ],
     packages=packages,
     scripts=[
     ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-getty/releases/tag/v0.03',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-getty/releases/tag/' + version,
     license='BSD')
